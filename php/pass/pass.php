c7b5f01d3811ead474de89451c7614621044b4e7
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-03-23 17:33:37 -0400

    Add files via upload

---
+++ php/pass/pass.php
@@ -0,0 +1,4 @@
+<?php
+	if ($_GET["pass"] == "Gavin is awesome, and I know it.") die("1");
+	die("0");
+?>
---
+++ self-xss-warning/index.js
@@ -0,0 +1,4 @@
+if (!top.window.gavinmorrow_com_self_xss_warning) {
+	console.log("\n%c WARNING! \n\n%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS. Do not enter or paste code that you do not understand. \nTo learn more about Self XSS, go to https://en.wikipedia.org/wiki/Self-XSS. \n", "font-size: 2vw;color: yellow;background-color: red;font-family: monospace;", "font-size: 1.5vw;color: white;background-color: black;font-family: monospace;");
+	top.window.gavinmorrow_com_self_xss_warning = true;
+}
