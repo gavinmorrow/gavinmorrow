80fe2974dee5a79edb601408e928e5567b148e10
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-19 09:13:27 -0400

    Start Remakaing Site

--- README.md
+++
@@ -1,2 +0,0 @@
-# gavinmorrow.com
-All the code for my website (http://gavinmorrow.com/) except for a few private things. 
--- php/multi/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$txt = $_POST["txt"];
-	$name = $_POST["name"];
-	$txtf = fopen("/home/gavin_unitytheory/gavinmorrow.com/multi/txt.txt", "a");
-	if (strlen($txt) > 0 and strlen($name) > 0) fwrite($txtf, "$txt -$name\n");
-	echo "<script>location.replace('/multi/');</script>";
-	fclose($txtf);
-?>
