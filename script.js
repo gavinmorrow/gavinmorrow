b776a8c4b373237b07d16dfd08279d97a4ada7ea
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-21 15:30:21 -0400

    Update Homepage

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ import UI from "https://gavinmorrow.github.io/EasyJS/1/ui/main.js";
 
 const {colorScheme, changeFavicon} = UI;
 
+let lastScrollEvent = Date.now();
+addEventListener("scroll", () => {
+	if (Date.now() >= lastScrollEvent + 10) {
+		const top = pageYOffset;
+		for (const img of document.body.querySelectorAll(".parallax-img")) {
+			
+		}
+
+		lastScrollEvent = Date.now();
+	}
+})
