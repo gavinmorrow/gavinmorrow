47b494d511e8d9cb50510db8adacad7d7db2be0d
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-21 10:17:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-import colorScheme from "https://gavinmorrow.github.io/EasyJS/1/ui/colorScheme/index.js";
+import UI from "https://gavinmorrow.github.io/EasyJS/1/ui/main.js";
 
-if (colorScheme);
+const {colorScheme, changeFavicon} = UI;
+
+colorScheme.set(false);
+if (colorScheme.dark) changeFavicon(document.querySelector("link[rel='shortcut icon']"), "//google.com/favicon.ico");
