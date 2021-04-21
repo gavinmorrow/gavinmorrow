540bc340ffdc3ae1b9fd3617b3a8fd85237a0625
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-21 16:38:10 -0400

    Parallax scrolling

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ import UI from "https://gavinmorrow.github.io/EasyJS/1/ui/main.js";
 
 const {colorScheme, changeFavicon} = UI;
 
-let lastScrollEvent = Date.now();
-addEventListener("scroll", () => {
-	if (Date.now() >= lastScrollEvent + 10) {
-		const top = pageYOffset;
-		for (const img of document.body.querySelectorAll(".parallax-img")) {
-			
-		}
-
-		lastScrollEvent = Date.now();
+const parallaxImgs = document.body.querySelectorAll(".parallax-img");
+const handleScroll = () => {
+	const top = scrollY;
+	for (const img of parallaxImgs) {
+		img.style.top = `calc(0px - ${top}px * (var(--speed) - 1) + var(--top))`;
 	}
-})
+};
+handleScroll();
+addEventListener("scroll", handleScroll);
