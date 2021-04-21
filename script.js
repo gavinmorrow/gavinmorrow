0927c474cdd0f43834b9038318c1ac61802cd699
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-21 11:16:00 -0400

    Update script.js
    Icons work in all color schemes, no need to change it.

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ import UI from "https://gavinmorrow.github.io/EasyJS/1/ui/main.js";
 
 const {colorScheme, changeFavicon} = UI;
 
-if (colorScheme.dark) changeFavicon(document.querySelector("link[rel='shortcut icon']"), "//google.com/favicon.ico");
